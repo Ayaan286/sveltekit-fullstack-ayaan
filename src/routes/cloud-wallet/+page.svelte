61ac4230a64b60e37cad722586b19c0a8fb03026
22053410 <ayaanpattanayak54@gmail.com>
2024-07-19 23:12:36 +0530

    initial

--- src/routes/cloud-wallet/+page.svelte
+++ src/routes/cloud-wallet/+page.svelte
@@ -15,7 +15,7 @@ export let form;
             >
               Email
             </label>
-            <input
+            <input  
               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="email"
               type="text"
